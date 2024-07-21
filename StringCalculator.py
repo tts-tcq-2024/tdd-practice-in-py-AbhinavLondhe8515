38d4060701259dd18e19f23e71d88cc54b83e3b2
Abhinav Londhe <59164413+AbhinavLondhe8515@users.noreply.github.com>
2024-07-21 21:53:52 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,24 +1,17 @@
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-
+    
     delimiter, numbers = parse_delimiter(numbers)
     numbers = numbers.replace("\n", delimiter)
     
-    total_sum = 0
-    for num in numbers.split(delimiter):
-        if num:
-            value = int(num)
-            if value <= 1000:
-                total_sum += value
-
-    return total_sum
+    return sum(
+        int(num) for num in numbers.split(delimiter) 
+        if num and int(num) <= 1000
+    )
 
 def parse_delimiter(numbers: str):
     if numbers.startswith("//"):
         parts = numbers.split("\n", 1)
-        delimiter = parts[0][2:]
-        numbers = parts[1]
-    else:
-        delimiter = ","
-    return delimiter, numbers
+        return parts[0][2:], parts[1]
+    return ",", numbers
